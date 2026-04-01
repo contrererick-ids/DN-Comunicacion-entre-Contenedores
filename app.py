0d6b7f9c9d0bc96f752016322f5918f3e189666b
contrererick-ids <is722185@iteso.mx>
2026-04-01 00:50:42 -0600

    Initialize project

---
+++ app.py
@@ -0,0 +1,21 @@
+# Práctica 4 - Comunicación entre contenedores
+# Alumno: Erick Alejandro Contreras Salas
+# Expediente: 722185
+# Desarrollo en la Nube - Primavera 2026 ITESO
+
+from fastapi import FastAPI
+from app.services.boletin_services import create_boletin, get_boletin_by_ID
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "¡Hola desde el contenedor de FastAPI!"}
+
+@app.get("/boletines/{boletin_id}")
+def get_boletin_endpoint(boletin_id: int):
+    return get_boletin_by_ID(boletin_id)
+
+@app.post("/boletines/{boletin_id}")
+def create_boletin_endpoint(boletin_id: int, boletin_message: str, boletin_file: bytes, email: str):
+    return create_boletin(boletin_id, boletin_message, boletin_file, email)
