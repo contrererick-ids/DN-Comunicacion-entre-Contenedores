68b28cb91a16786fe66f7cc29c6b63236ef27f2c
contrererick-ids <is722185@iteso.mx>
2026-04-03 00:37:51 -0600

    Refactor POST /boletines endpoint

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Expediente: 722185
 # Desarrollo en la Nube - Primavera 2026 ITESO
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
 from app.services.boletin_services import create_boletin
 
 app = FastAPI()
@@ -12,6 +12,11 @@ app = FastAPI()
 def read_root():
     return {"message": "¡Hola desde el contenedor de FastAPI!"}
 
-@app.post("/boletines/{boletin_id}")
-def create_boletin_endpoint(boletin_id: int, boletin_message: str, boletin_file: bytes, email: str):
-    return create_boletin(boletin_id, boletin_message, boletin_file, email)
+@app.post("/boletines")
+async def create_boletin_endpoint(
+    boletinFile: UploadFile = File(...),
+    boletinMessage: str = Form(...),
+    email: str = Form(...)
+):
+    boletinFileContent = await boletinFile.read()
+    return create_boletin(boletinFileContent, boletinMessage, email)
